45a5145626f2d7efdaeb4c3087265a9aa6a13e70
alpha <peker.celik@ozu.edu.tr>
2023-07-14 03:35:20 +0300

    works

--- spider.py
+++ spider.py
@@ -1,19 +1,23 @@
 import json
-import time
-import urllib.request
-from urllib.error import HTTPError
 import logging
 import pika
+from pymongo import MongoClient
+import urllib.request
+from urllib.error import HTTPError
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 
 class Spider:
-    def __init__(self, message_queue_url):
+    def __init__(self, message_queue_url, mongodb_url, database_name):
         self.message_queue_url = message_queue_url
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=message_queue_url))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue='spider_queue')
+        self.channel.queue_declare(queue='shopify')
         self.logger = self.setup_logging()
+
+        self.mongo_client = MongoClient(mongodb_url)
+        self.db = self.mongo_client[database_name]
+        self.collections_collection = self.db['collections']
 
     def setup_logging(self):
         logger = logging.getLogger(__name__)
@@ -25,88 +29,103 @@ class Spider:
         logger.addHandler(handler)
         return logger
 
-    def get_collections(self, url):
-        # Same implementation as before, but modify it to send messages to the message queue instead of returning collections directly
-        collections_url = url + 'collections.json'
-        page = 1
-        while True:
-            # ...
-            # Instead of returning collections, send messages to the message queue
-            self.channel.basic_publish(
-                exchange='',
-                routing_key='spider_queue',
-                body=json.dumps({
-                    'url': url,
-                    'collection_page': page
-                })
-            )
-            # ...
-
-    def get_products_of_collection(self, url, collection):
-        # Same implementation as before, but modify it to send messages to the message queue instead of returning products directly
-        page = 1
-        while True:
-            # ...
-            # Instead of returning products, send messages to the message queue
-            self.channel.basic_publish(
-                exchange='',
-                routing_key='spider_queue',
-                body=json.dumps({
-                    'url': url,
-                    'collection': collection,
-                    'product_page': page
-                })
-            )
-            # ...
-
     def isShopify(self, url):
-        # Same implementation as before, but modify it to return a boolean indicating if the URL is valid
         try:
-            self.get_collections(url)  # Modify this to use the new method for sending messages
+            req = urllib.request.Request(url + '/collections.json', headers={'User-Agent': USER_AGENT})
+            urllib.request.urlopen(req)
             return True
         except:
             return False
 
     def callback(self, ch, method, properties, body):
-        task = json.loads(body)
-        url = task['url']
-        if 'collection_page' in task:
-            collection_page = task['collection_page']
-            self.logger.info(f"Retrieving collections for URL: {url}, page: {collection_page}")
-            if self.isShopify(url):
-                collections = self.retrieve_collections(url, collection_page)
-                self.logger.info(f"Retrieved collections for URL: {url}, page: {collection_page}")
-                for collection in collections:
-                    self.get_products_of_collection(url, collection)
-            else:
-                self.logger.warning(f"Invalid URL: {url}")
-        elif 'product_page' in task:
-            collection = task['collection']
-            product_page = task['product_page']
-            self.logger.info(f"Retrieving products for URL: {url}, collection: {collection}, page: {product_page}")
-            if self.isShopify(url):
-                products = self.retrieve_products(url, collection, product_page)
-                self.logger.info(f"Retrieved products for URL: {url}, collection: {collection}, page: {product_page}")
-            else:
-                self.logger.warning(f"Invalid URL: {url}")
+        url = body.decode()
+        self.logger.info(f"Received URL: {url}")
+        if self.isShopify(url):
+            self.logger.info(f"Valid URL: {url}")
+            self.get_collections(url)
+        else:
+            self.logger.warning(f"Invalid URL: {url}")
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     def start_consuming(self):
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue='spider_queue', on_message_callback=self.callback)
+        self.channel.basic_consume(queue='shopify', on_message_callback=self.callback)
         self.channel.start_consuming()
 
-    def retrieve_collections(self, url, page):
-        # Modify this method to retrieve collections from the Shopify API based on the given URL and page
-        # Return the collections instead of sending messages to the message queue
-        pass
-    def retrieve_products(self, url, collection, page):
-        # Modify this method to retrieve products from the Shopify API based on the given URL, collection, and page
-        # Return the products instead of sending messages to the message queue
-        pass
+    def get_collections(self, url):
+        collections = self.retrieve_collections(url)
+        if collections:
+            doc = {
+                'url': url,
+                'collections': collections,
+            }
+            self.collections_collection.update_one({'url': url}, {'$set': doc}, upsert=True)
+            for collection in collections:
+                collection_name = collection.get('handle', '')
+                if collection_name:
+                    self.collections_collection.update_one(
+                        {'url': url},
+                        {'$addToSet': {'collections': {'$each': [collection]}}},
+                    )
+                    self.get_products_of_collection(url, collection_name)
+        
+    def get_products_of_collection(self, url, collection_name):
+        products = self.retrieve_products(url, collection_name)
+        if products:
+            self.collections_collection.update_one(
+                {'url': url, 'collections.handle': collection_name},
+                {'$push': {'collections.$.products': {'$each': products}}},
+            )
+    
 
+    
+        
+    
+
+    def retrieve_collections(self, url):
+        collections_url = f"{url}/collections.json"
+        req = urllib.request.Request(collections_url, headers={'User-Agent': USER_AGENT})
+        try:
+            response = urllib.request.urlopen(req)
+            response_data = response.read().decode('utf-8')
+            if response_data:
+                collections_data = json.loads(response_data)
+                collections = collections_data.get('collections', [])
+                return collections
+            else:
+                self.logger.warning(f"Empty response while retrieving collections from URL: {url}")
+                return []
+        except HTTPError as e:
+            self.logger.error(f"Error retrieving collections: {e}")
+            return []
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Error decoding JSON response while retrieving collections from URL: {url}: {e}")
+            return []
+
+    def retrieve_products(self, url, collection_name):
+        products_url = f"{url}/collections/{collection_name}/products.json"
+        req = urllib.request.Request(products_url, headers={'User-Agent': USER_AGENT})
+        try:
+            response = urllib.request.urlopen(req)
+            response_data = response.read().decode('utf-8')
+            if response_data:
+                products_data = json.loads(response_data)
+                products = products_data.get('products', [])
+                return products
+            else:
+                self.logger.warning(f"Empty response while retrieving products from URL: {url}, collection name: {collection_name}")
+                return []
+        except HTTPError as e:
+            self.logger.error(f"Error retrieving products: {e}")
+            return []
+        except json.JSONDecodeError as e:
+            self.logger.error(f"Error decoding JSON response while retrieving products from URL: {url}, collection name: {collection_name}: {e}")
+            return []
 
 if __name__ == '__main__':
     message_queue_url = 'localhost'  # Replace with the actual message queue URL
-    spider = Spider(message_queue_url)
+    mongodb_url = 'mongodb://localhost:27017/'  # Replace with the actual MongoDB URL
+    database_name = 'shopify'  # Replace with the name of your MongoDB database
+
+    spider = Spider(message_queue_url, mongodb_url, database_name)
     spider.start_consuming()
